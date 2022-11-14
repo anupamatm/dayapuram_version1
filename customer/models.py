a9829850a962790f94f35efe17c57879df09d004
suvarna <suvarna007bars@gmail.com>
2022-11-14 15:24:23 +0530

    customer tables payment order details address

--- customer/models.py
+++ customer/models.py
@@ -1,6 +1,6 @@
 from email.policy import default
 from django.db import models
-
+from datetime import date
 from reseller_app.models import Product
 
 
@@ -17,3 +17,34 @@ class AddCart(models.Model):
     product  = models.ForeignKey(Product, on_delete = models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
     qty = models.IntegerField(default=1)
+
+class Addresslist(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    selected_address = models.CharField(max_length=200)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    productid = models.ForeignKey(Product,on_delete=models.CASCADE)
+    address = models.ForeignKey(Addresslist,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20,default="placed") #update after payment confirmed
+    
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    total_amount = models.FloatField()
+    date = models.DateField(default=date.today)
+    p_status = models.CharField(max_length=20,default="fail")
+    
+
+class Order_details(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    address = models.ForeignKey(Addresslist,on_delete=models.CASCADE)
+
+
+ 
+
+
+
