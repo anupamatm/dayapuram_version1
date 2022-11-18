ce673ed62827d792fe3e299379ab81bc495525ff
suvarna <suvarna007bars@gmail.com>
2022-11-18 17:34:46 +0530

    reseller admin cards checkout

--- customer/models.py
+++ customer/models.py
@@ -18,39 +18,13 @@ class AddCart(models.Model):
     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
     qty = models.IntegerField(default=1)
 
-class Addresslist(models.Model):
-    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
-    selected_address = models.CharField(max_length=200)
-
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
     productid = models.ForeignKey(Product,on_delete=models.CASCADE)
-    address = models.ForeignKey(Addresslist,on_delete=models.CASCADE)
+    address = models.CharField(max_length=200, default="")
     quantity = models.IntegerField()
-    status = models.CharField(max_length=20,default="placed") #update after payment confirmed
-    
-
-class Payment(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
-    total_amount = models.FloatField()
+    status = models.CharField(max_length=20,default="") #update after payment confirmed
     date = models.DateField(default=date.today)
-    p_status = models.CharField(max_length=20,default="fail")
     
-
-class Order_details(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
-    payment = models.ForeignKey(Payment,on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-    address = models.ForeignKey(Addresslist,on_delete=models.CASCADE)
-
-
-# class Order(models.Model):
-#     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-#     address = models.ForeignKey(Addresslist,on_delete=models.CASCADE)
-#     quantity = models.IntegerField()
-#     status = models.CharField(max_length=20,default="placed") #update after payment confirmed
-    
-
 
 
